dc026f3e6dc1a3b8f81caaae4416e2381098a4d4
lkaratun <lev.karatun@gmail.com>
2018-11-11 22:53:46 -0500

    refactor

--- tests/main.test.js
+++ tests/main.test.js
@@ -20,40 +20,30 @@ describe("app", () => {
     expect(app.libraryBooks).toHaveLength(3);
   });
 });
+
 describe("addNewBookToBookList", () => {
   beforeEach(async () => {
     const dom = await JSDOM.fromFile("e2_library.html");
     ({ document } = dom.window);
     app = require("../e2_library")(document);
-  });
-
-  it("adds an entry to the libraryBooks array", () => {
-    const myForm = document.querySelector("#bookAddForm");
-    myForm.querySelector("#newBookName").value = "Test name";
-    myForm.querySelector("#newBookAuthor").value = "Test author";
-    myForm.querySelector("#newBookGenre").value = "Test genre";
+    const bookAddForm = document.querySelector("#bookAddForm");
+    bookAddForm.querySelector("#newBookName").value = "Test name";
+    bookAddForm.querySelector("#newBookAuthor").value = "Test author";
+    bookAddForm.querySelector("#newBookGenre").value = "Test genre";
     app.addNewBookToBookList(e);
+  });
+  it("adds an entry to the libraryBooks array", () => {
     expect(app.libraryBooks).toHaveLength(4);
     expect(typeof app.libraryBooks[3]).toBe("object");
     expect(app.libraryBooks[3] instanceof app.Book).toBeTruthy();
   });
   it("new entry in the libraryBooks array has correct properties", () => {
-    const myForm = document.querySelector("#bookAddForm");
-    myForm.querySelector("#newBookName").value = "Test name";
-    myForm.querySelector("#newBookAuthor").value = "Test author";
-    myForm.querySelector("#newBookGenre").value = "Test genre";
-    app.addNewBookToBookList(e);
     expect(app.libraryBooks[3].title).toBe("Test name");
     expect(app.libraryBooks[3].author).toBe("Test author");
     expect(app.libraryBooks[3].genre).toBe("Test genre");
     expect(app.libraryBooks[3].bookId).toBe(3);
   });
   it("patron card number is blank when first adding the book", () => {
-    const myForm = document.querySelector("#bookAddForm");
-    myForm.querySelector("#newBookName").value = "Test name";
-    myForm.querySelector("#newBookAuthor").value = "Test author";
-    myForm.querySelector("#newBookGenre").value = "Test genre";
-    app.addNewBookToBookList(e);
     expect(app.libraryBooks[3].patron).toBe(null);
   });
 });
